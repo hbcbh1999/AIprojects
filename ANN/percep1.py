8d72c7ffa257d577b46447017996554534797763
JuanSensio+ <juansensio03@gmail.com>
2017-11-07 13:34:00 +0100

    commit

--- ANN/percep1.py
+++ ANN/percep1.py
@@ -8,9 +8,9 @@ def main():
 
     # train data
     data = {}
-    data["dim1"] = [0.9,2,2,1.1,1]
-    data["dim2"] = [0.1,0.8,0.9,0.2,0.1]
-    data["class"] = [-1,1,1,-1,-1]
+    data["dim1"] = [0.9,2,2,1.1,1,1.5]
+    data["dim2"] = [0.1,0.8,0.9,0.2,0.1,0.2]
+    data["class"] = [-1,1,1,-1,-1,1]
 
     # train perceptron
     nn = Perceptron(ploter=True) 
@@ -22,7 +22,7 @@ def main():
         x[i][1] = data["dim2"][i]
     nn.train(x)          
     # plot results
-    # nn.plot(x,'final')
+    nn.plot(x,'final')
 
     # test perceptron
     testData = np.asarray([1.8,0.9])
@@ -31,7 +31,7 @@ def main():
     plt.plot(testData[0],testData[1],'.b')
 
     # plot results
-    nn.plot(x,'test')
+    # nn.plot(x,'test')
 
     
 # main
